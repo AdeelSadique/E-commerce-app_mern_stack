01b2378dd9fff8e646981367ab18ac5cc104cf1d
AdeelSadique <adeelsadique0@hotmail.com>
2024-03-05 16:05:15 +0500

    vercel.json updated

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: '*' }));
 app.use('/api', product);
