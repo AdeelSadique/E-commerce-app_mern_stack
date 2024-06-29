8bbaa7cf59981a43ce4679e074a925e48397f6aa
AdeelSadique <adeelsadique0@hotmail.com>
2024-06-29 15:03:22 +0500

    url changing in frontend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: 'https://e-commerce-app-mern-stack-47dv-dwc9t1b50-adeelsadiques-projects.vercel.app/',
+    origin: '*',
   })
 );
 app.use('/api', product);
