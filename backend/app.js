363ba6cef194eb6fe018c24355b386feed517052
AdeelSadique <adeelsadique0@hotmail.com>
2024-06-30 11:41:02 +0500

    url changing in frontend

--- backend/app.js
+++ backend/app.js
@@ -19,37 +19,14 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: [
-      'https://e-commerce-app-mern-stack-47dv-66kfq9hj1-adeelsadiques-projects.vercel.app/',
-      'https://e-commerce-app-mern-stackbackend-77torjnf8.vercel.app/',
-    ],
+    origin: 'https://e-commerce-app-mern-stack-47dv-66kfq9hj1-adeelsadiques-projects.vercel.app/',
   })
 );
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: '*',
-//   })
-// );
-
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-// res.setHeader(
-//   'Access-Control-Allow-Headers',
-//   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-// )
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   res.setHeader('Access-Control-Allow-Origin', 'https://e-commerce-app-mern-stack-47dv-66kfq9hj1-adeelsadiques-projects.vercel.app');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   );
-// });
 
 app.use('/api', product);
+app.use('/api/testing', (req, res) => {
+  res.json({ msg: 'testing complete' });
+});
 app.use('/api', userRoutes);
 app.use('/api', cartRoutes);
 app.use('/api', orderRoutes);
