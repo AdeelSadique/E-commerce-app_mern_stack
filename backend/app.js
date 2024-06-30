a87ea6c1a7d6d7944171c58e06b1438d804a8ab8
AdeelSadique <adeelsadique0@hotmail.com>
2024-06-30 19:14:27 +0500

    url changing in frontend and setting env vars

--- backend/app.js
+++ backend/app.js
@@ -25,9 +25,9 @@ app.use(
 
 app.use('/api', product);
 
-app.use('api', userRoutes);
-app.use('api', cartRoutes);
-app.use('api', orderRoutes);
+app.use('/api', userRoutes);
+app.use('/api', cartRoutes);
+app.use('/api', orderRoutes);
 app.use(errorMiddleware);
 
 module.exports = app;
