ff3966d396761177ae600e4e6a54362cdb17ef49
AdeelSadique <adeelsadique0@hotmail.com>
2024-07-03 01:31:06 +0500

    sameSite:'None'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,10 +51,11 @@ exports.login = async (req, res, next) => {
         } else {
           const token = jwt.sign({ id: user._id }, process.env.JWTSECRETKEY);
           const cookieOptions = {
-            httpOnly: true,
+            // httpOnly: true,
             expires: new Date(Date.now() + 60 * 60 + 6 * 1000),
             sameSite: 'None',
             secure: true,
+            path: '/',
           };
 
           // cookie setted for 6 hour
