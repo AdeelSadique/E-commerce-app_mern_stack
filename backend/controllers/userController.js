6f43115b805d624f02da179f86c5a41087d8d08d
AdeelSadique <adeelsadique0@hotmail.com>
2024-07-05 00:33:32 +0500

    cookie deleting error resolving

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,11 +55,11 @@ exports.login = async (req, res, next) => {
           const token = jwt.sign({ id: user._id }, process.env.JWTSECRETKEY);
           const cookieOptions = {
             expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-            // httpOnly: true,
+            httpOnly: true,
             path: '/',
             sameSite: 'None',
             secure: true,
-            domain: new URL(process.env.BACKEND_URL).hostname,
+            domain: 'https://e-commerce-mern-backend-theta.vercel.app/',
             // maxAge: new Date(Date.now() + 1 * 60 * 60 * 1000),
           };
 
@@ -88,11 +88,11 @@ exports.logout = async (req, res, next) => {
   try {
     const options = {
       expires: new Date(0),
-      // httpOnly: true,
+      httpOnly: true,
       sameSite: 'None',
       secure: true,
       path: '/',
-      domain: new URL(process.env.BACKEND_URL).hostname,
+      domain: 'https://e-commerce-mern-backend-theta.vercel.app/',
     };
 
     res.clearCookie('token', options).status(200).json({
